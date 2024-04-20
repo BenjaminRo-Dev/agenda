4a37fe7ad90a0316a82f6c078d8f65005cd2b10f
BenjaminRo-Dev <programador.ben@gmail.com>
2024-04-20 00:38:30 -0400

    Login con token

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,60 +4,56 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //Login:
-    public function login(Request $request){
-        try {
-            $user = User::where('email', $request->email)->first();
-            if($user){
 
-                if (Auth::attempt($request->only('email', 'password'))) {
-                    $user = Auth::user();
-                    $token = $request->user()->createToken($request->email.'_Token');
-                    
-                    return response()->json([
-                        'message' => 'Login success',
-                        'user' => $user,
-                        'token' => $token->plainTextToken,
-                        'status_code' => 200,
-                    ], 200);
-                }else{
-                    return response()->json([
-                    'message' => 'Password incorrect',
-                    'status_code' => 401,
-                ], 401);
-                }
-            }else{
-                return response()->json([
-                    'message' => 'User not found',
-                    'status_code' => 404,
-                ], 404);
-            }
-        } catch (\Exception $e) {
-            return response()->json([
-                'message' => 'Error: '.$e->getMessage(),
-                // 'message' => 'Error interno del servidor',
-                'status_code' => 500,
-            ], 500);
-        }
+    public function validateToken(Request $request)
+    {
+        $token = $request->headers->get('Authorization');
+        return response()->json([
+            'message' => 'Token valido',
+            'status_code' => 200,
+            'token' => $token,
+        ], 200);
     }
+
+    //Logout:
+    public function logout(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        $user->tokens()->delete();
+        return response()->json([
+            'message' => 'Sesion cerrada',
+            'status_code' => 200,
+        ], 200);
+    }
+
+
+    //Login:
+
+    public function login(Request $request)
+    {
+        if(!Auth::attempt($request->only('email', 'password'))){
+            return response()->json([
+                'message' => 'Credenciales incorrectas',
+                'status_code' => 401,
+            ], 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+        $token = $user->createToken($request->email.'_Token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login exitoso',
+            'status_code' => 200,
+            'token' => $token,
+            'user' => $user,
+        ], 200);
+    }
+
 }
 
-// $data = $request->all();
-//         $user = User::where('email', $data['email'])->first();
-//         if($user && password_verify($data['password'], $user->password)){
-//             return response()->json([
-//                 'status_code' => 200,
-//                 'message' => 'Login exitoso',
-//                 'data' => $user
-//             ], 200);
-//         }else{
-//             return response()->json([
-//                 'status_code' => 400,
-//                 'message' => 'Credenciales incorrectas'
-//             ], 400);
-//         }
--- routes/api.php
+++ routes/api.php
@@ -13,17 +13,25 @@ Route::get('/', function () {
     return response()->json(['message' => 'Hello World!']);
 });
 
-
-// Route::apiResource('materias', MateriaController::class)->names('api.materias');
-Route::get('materias/estudiante/{estudiante_id}', [MateriaEstudianteController::class, 'index'])->name('api.materias.estudiate.index');
-Route::get('materias/profesor/{profesor_id}', [MateriaProfesorController::class, 'index'])->name('api.materias.profesor.index');
-
-//Profesor:
-Route::get('profesor/datos-personales/{idProfesor}', [ProfesorController::class, 'datosPersonales'])->name('api.profesor.datos-personales');
-Route::get('profesor/cursos/{idProfesor}', [ProfesorController::class, 'cursos'])->name('api.profesor.cursos');
-Route::get('profesor/estudiantes-curso/{idProfesor}', [ProfesorController::class, 'estudiantesCurso'])->name('api.profesor.estudiantes-curso');
-
-
-//Login:
 Route::post('login', [UserController::class, 'login'])->name('api.login');
+
+Route::middleware('auth:sanctum')->group(function (){
+    
+    // Route::apiResource('materias', MateriaController::class)->names('api.materias');
+    Route::get('materias/estudiante/{estudiante_id}', [MateriaEstudianteController::class, 'index'])->name('api.materias.estudiate.index');
+    Route::get('materias/profesor/{profesor_id}', [MateriaProfesorController::class, 'index'])->name('api.materias.profesor.index');
+
+    //Profesor:
+    Route::get('profesor/datos-personales/{idProfesor}', [ProfesorController::class, 'datosPersonales'])->name('api.profesor.datos-personales');
+    Route::get('profesor/cursos/{idProfesor}', [ProfesorController::class, 'cursos'])->name('api.profesor.cursos');
+    Route::get('profesor/estudiantes-curso/{idProfesor}', [ProfesorController::class, 'estudiantesCurso'])->name('api.profesor.estudiantes-curso');
+
+
+    //Login:
+    
+    Route::post('validate-token', [UserController::class, 'validateToken'])->name('api.validate-token');
+    Route::post('logout', [UserController::class, 'logout'])->name('api.logout');
+
+
+});
 
